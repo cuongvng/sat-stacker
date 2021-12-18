d67586fbe36d9211a0d08760eee96fdd8400b3ec
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-12-18 03:07:20 -0500

    get acc info

--- main.py
+++ main.py
@@ -14,13 +14,22 @@ SAT_STACKER_API_SECRET = os.getenv("SAT_STACKER_API_SECRET")
 BASE_ENDPOINT = "https://ftx.com/api"
 SUB_ACCOUNT = os.getenv("SUB_ACCOUNT")
 
-def main():
-	# Authenticate
+def authenticate(method, end_point_path):
 	ts = int(time.time() * 1000)
-
-	signature_payload = f'{ts}'.encode()
+	signature_payload = f'{ts}{method}/api{end_point_path}'.encode()
 	signature = hmac.new(SAT_STACKER_API_SECRET.encode(), signature_payload, 'sha256').hexdigest()
+	return ts, signature
 
+def place_spot_market_order():
+	pass
+
+def get_account_info():
+	# Authenticate
+	METHOD = "GET"
+	END_POINT_PATH = "/account"
+	ts, signature = authenticate(METHOD, END_POINT_PATH)
+
+	# Request
 	headers = {}
 	
 	headers['FTX-KEY'] = SAT_STACKER_API_KEY
@@ -28,9 +37,13 @@ def main():
 	headers['FTX-TS'] = str(ts)
 	headers['FTX-SUBACCOUNT'] = SUB_ACCOUNT
 
-	response = requests.get(BASE_ENDPOINT+"/markets/BTC/USD", headers=headers)
-	print(response.json())
-
+	response = requests.get(BASE_ENDPOINT+END_POINT_PATH, headers=headers)
+	
+	if response.status_code == 200:
+		print(response.json()["result"])
+	else:
+		print("Error:", response.json()["error"])
+	
 
 if __name__ == "__main__":
-	main()
+	get_account_info()
