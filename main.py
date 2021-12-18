68e554b3c124410ed815b8bf3ac09f3335ef5b00
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-12-18 06:58:39 -0500

    unresolved error: missing param market

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from dotenv import load_dotenv
 import os
-
-import requests 
 load_dotenv()
 
+import requests 
 import time
 import hmac
 import json
-from requests import Request
 
 SAT_STACKER_API_KEY = os.getenv("SAT_STACKER_API_KEY")
 SAT_STACKER_API_SECRET = os.getenv("SAT_STACKER_API_SECRET")
 BASE_ENDPOINT = "https://ftx.com/api"
 SUB_ACCOUNT = os.getenv("SUB_ACCOUNT")
+EMAIL = os.getenv("EMAIL")
 
-def authenticate(method, end_point_path):
+def authenticate(method, end_point_path, payload=None):
 	ts = int(time.time() * 1000)
 	signature_payload = f'{ts}{method}/api{end_point_path}'.encode()
+	if payload:
+		signature_payload = f'{ts}{method}/api{end_point_path}{payload}'.encode()
+
 	signature = hmac.new(SAT_STACKER_API_SECRET.encode(), signature_payload, 'sha256').hexdigest()
 	return ts, signature
-
-def place_spot_market_order():
-	pass
 
 def get_account_info():
 	# Authenticate
@@ -31,7 +30,6 @@ def get_account_info():
 
 	# Request
 	headers = {}
-	
 	headers['FTX-KEY'] = SAT_STACKER_API_KEY
 	headers['FTX-SIGN'] = signature
 	headers['FTX-TS'] = str(ts)
@@ -44,6 +42,47 @@ def get_account_info():
 	else:
 		print("Error:", response.json()["error"])
 	
+def stack_sats():
+	"""
+	POST signature example (from https://blog.ftx.com/blog/api-authentication/):
+
+	signature_payload = b'1588591856950POST/api/orders{"market": "BTC-PERP", "side": "buy", "price": 8500, "size": 1, "type": "limit", "reduceOnly": false, "ioc": false, "postOnly": false, "clientId": null}'
+	"""
+
+	# Place a spot market order
+	MIN_ORDER_SIZE = 0.0001
+	payload = json.dumps({
+		"market": "BTC/USD",
+		"side": "buy",
+		"price": None,
+		"size": MIN_ORDER_SIZE,
+		"type": "market",
+		"reduceOnly": False,
+		"ioc": False,
+		"postOnly": False,
+		"clientId": None
+	})
+	# Authenticate
+	METHOD = "POST"
+	END_POINT_PATH = "/orders"
+	ts, signature = authenticate(METHOD, END_POINT_PATH, payload)
+
+	# Request
+	headers = {
+		'FTX-KEY': SAT_STACKER_API_KEY,
+		'FTX-SIGN': signature,
+		'FTX-TS': str(ts),
+		'FTX-SUBACCOUNT': SUB_ACCOUNT
+	}
+
+	response = requests.post(BASE_ENDPOINT+END_POINT_PATH, data=payload, headers=headers)
+	if response.status_code == 200:
+		print(response.json()["result"])
+	else:
+		# Send me an email
+		print("Error:", response.json()["error"])
+		print(response.json())
 
 if __name__ == "__main__":
-	get_account_info()
+	# get_account_info()
+	stack_sats()
