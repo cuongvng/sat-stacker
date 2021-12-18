86a424c86320f84d253622456c72639225bd8924
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-12-18 02:47:56 -0500

    authenticate

---
+++ main.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import os
+
+import requests 
+load_dotenv()
+
+import time
+import hmac
+import json
+from requests import Request
+
+SAT_STACKER_API_KEY = os.getenv("SAT_STACKER_API_KEY")
+SAT_STACKER_API_SECRET = os.getenv("SAT_STACKER_API_SECRET")
+FTX_REST_ENDPOINT = "https://ftx.com/api"
+SUB_ACCOUNT = os.getenv("SUB_ACCOUNT")
+
+def main():
+	# Authenticate
+	ts = int(time.time() * 1000)
+
+	signature_payload = f'{ts}GET/api/markets'.encode()
+	signature = hmac.new(SAT_STACKER_API_SECRET.encode(), signature_payload, 'sha256').hexdigest()
+
+	headers = {}
+	
+	headers['FTX-KEY'] = SAT_STACKER_API_KEY
+	headers['FTX-SIGN'] = signature
+	headers['FTX-TS'] = str(ts)
+	headers['FTX-SUBACCOUNT'] = SUB_ACCOUNT
+
+	response = requests.get(FTX_REST_ENDPOINT, headers=headers)
+	print(response.json())
+
+
+if __name__ == "__main__":
+	main()
